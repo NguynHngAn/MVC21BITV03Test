7d060c1f44f76c8ccad2d7a3076ef82e33318d9b
LyDat123 <lydat12a1@gmail.com>
2024-08-09 16:04:47 +0700

    fix bug Loi database khong nhan

--- WWorldMidTest/obj/Debug/net8.0/WWorldMidTest.AssemblyInfo.cs
+++ WWorldMidTest/obj/Debug/net8.0/WWorldMidTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WWorldMidTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c2786b83b5f2dc728bd2122fa263e99ddb96aa3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07bc5b8b179394b5ce7b905f64c5f27c382cc88f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WWorldMidTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WWorldMidTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
